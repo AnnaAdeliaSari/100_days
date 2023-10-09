d4d514c2b12df2f820c47a4d0c373a093c28a74f
AnnaAdeliaSari <147259007+AnnaAdeliaSari@users.noreply.github.com>
2023-10-09 20:47:11 +0800

    day 1
    membuat operasi penjumlahan dua variabel integer (bilangan bulat)

---
+++ hitung.java
@@ -0,0 +1,14 @@
+
+package day1;
+
+
+public class hitung {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 4;
+        int c = a + b;
+        
+        System.out.println(c);
+    }
+    
+}
